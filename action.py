f4f09817e84870cf29770e2b9d9cde82a25808c6
Ellie The Yeen <42704150+EllieTheYeen@users.noreply.github.com>
2023-11-15 12:39:29 +0100

    Test something wild

--- action.py
+++ action.py
@@ -73,6 +73,7 @@ if __name__ == "__main__":
         exit()
 
     print_and_run(f"git switch -f -C {shlex.quote(args.branch2)}")
+    print_and_run(f"git pull origin {shlex.quote(args.branch2)} --depth 1")
 
     msg = (
         f"New post: {found}\n{args.blogbase}/{found.replace('-', '/', 3)}.html"
