23fabb90628972cd22dae09d62dc89e51e3a8e34
Ellie The Yeen <42704150+EllieTheYeen@users.noreply.github.com>
2023-11-15 13:03:00 +0100

    Without the pull?

--- action.py
+++ action.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     print_and_run(f"git config --global user.name {q(args.name)}")
     print_and_run(f"git config --global user.email {q(args.email)}")
-    print_and_run("git config --global pull.ff only")
+    #print_and_run("git config --global pull.ff only")
     print_and_run(f"git switch -f -C {q(args.branch2)}")
 
     post_file = "testposts.csv"
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 
     print_and_run(f"git switch -f -C {q(args.branch2)}")
     print_and_run("git reset --hard")
-    print_and_run(f"git pull origin {q(args.branch2)} --depth 1")
+    #print_and_run(f"git pull origin {q(args.branch2)} --depth 1")
 
     msg = (
         f"New post: {found}\n{args.blogbase}/{found.replace('-', '/', 3)}.html"
